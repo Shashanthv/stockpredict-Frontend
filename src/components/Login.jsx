9de3c4898fb4eb9c8774814af5d7d8846a08caa3
shashisuryavs <142174390+shashisuryavs@users.noreply.github.com>
2025-04-09 19:56:10 +0530

    #

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://stockwisely.onrender.com/login', {
+      const response = await axios.post('https://stockwisely.onrender.com', {
         email,
         password,
       });
